8d314c79636e387beb142365bcca15e029abc9c0
nametake <nametake.kyarabuki@gmail.com>
2021-05-22 23:27:24 +0900

    Implemented BufferPool

--- src/buffer_pool.rs
+++ src/buffer_pool.rs
@@ -1,9 +1,11 @@
 use std::{
     cell::{Cell, RefCell},
+    collections::HashMap,
+    ops::Index,
     rc::Rc,
 };
 
-use crate::disk_manager::{PageId, PAGE_SIZE};
+use crate::disk_manager::{DiskManager, PageId, PAGE_SIZE};
 
 pub struct BufferId(usize);
 
@@ -15,6 +17,16 @@ pub struct Buffer {
     pub is_dirty: Cell<bool>,
 }
 
+impl Default for Buffer {
+    fn default() -> Self {
+        Self {
+            page_id: Default::default(),
+            page: RefCell::new([0u8; PAGE_SIZE]),
+            is_dirty: Cell::new(false),
+        }
+    }
+}
+
 pub struct Frame {
     usage_count: u64,
     buffer: Rc<Buffer>,
@@ -23,4 +35,63 @@ pub struct Frame {
 pub struct BufferPool {
     buffers: Vec<Frame>,
     next_victim_id: BufferId,
+}
+
+impl BufferPool {
+    pub fn new(pool_size: usize) -> Self {
+        let mut buffers = vec![];
+        buffers.resize_with(pool_size, Default::default);
+        let next_victim_id = BufferId::default();
+        Self {
+            buffers,
+            next_victim_id,
+        }
+    }
+
+    fn evict(&mut self) -> Option<BufferId> {
+        let pool_size = self.size();
+        let mut consecutive_pinned = 0;
+
+        let victim_id = loop {
+            let next_victim_id = self.next_victim_id;
+            let frame = &mut self[next_victim_id];
+            if frame.usage_count == 0 {
+                break self.next_victim_id;
+            }
+
+            if Rc::get_mut(&mut frame.buffer).is_some() {
+                frame.usage_count -= 1;
+                consecutive_pinned = 0;
+            } else {
+                consecutive_pinned += 1;
+                if consecutive_pinned >= pool_size {
+                    return None;
+                }
+            }
+            self.next_victim_id = self.increment_id(self.next_victim_id);
+        };
+        Some(victim_id)
+    }
+
+    fn size(&mut self) -> usize {
+        todo!()
+    }
+
+    fn increment_id(&self, buffer_id: BufferId) -> BufferId {
+        todo!()
+    }
+}
+
+impl Index<BufferId> for BufferPool {
+    type Output = Frame;
+
+    fn index(&self, index: BufferId) -> &Self::Output {
+        &self.buffers[index.0]
+    }
+}
+
+pub struct BufferPoolManager {
+    disk: DiskManager,
+    pool: BufferPool,
+    page_table: HashMap<PageId, BufferId>,
 }
--- src/buffer_pool_manager.rs
+++
@@ -1,6 +0,0 @@
-use crate::disk_manager::DiskManager;
-
-pub struct BufferPoolManager {
-    disk: DiskManager,
-    pool: BufferP,
-}
